9c1058ad19ea4b2ea1483f87c29893c2265641df
ThuanNTV <thuanntv721@gmail.com>
2025-05-24 09:18:28 +0700

    refactor(notifier.py): streamline telegram message formatting logic

--- crawler/jobhub_crawler/utils/notifier.py
+++ crawler/jobhub_crawler/utils/notifier.py
@@ -11,30 +11,42 @@ logger = logging.getLogger(__name__)
 
 
 def _send_telegram_message(crawl_time, file_path, total_records, elapsed_time, error_count):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": f"""
-    🚀 *Crawl Job Status: Hoàn thành thành công!*
-    
-    🕒 Thời gian: `{crawl_time}`
-    📄 File kết quả: `{file_path}`
-    ✅ Tổng số bản ghi: *{total_records}*
-    
-    ⚙️ Thông tin bổ sung:
-    - Thời gian chạy: {elapsed_time} giây
-    - Số lỗi phát sinh: {error_count}
-    
-    💡 Lưu ý:
-    - Kiểm tra kỹ dữ liệu để đảm bảo tính chính xác.
-    - Nếu cần hỗ trợ, liên hệ team dev.
-    
-    Cảm ơn bạn đã sử dụng bot crawl tự động!
-     
-    🔗 Tải file: [Download tại đây]👇 
-    """,
-        "parse_mode": "Markdown"
-    }
+    if crawl_time == "":
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": f"""
+                    Nội dung tin nhắn: 
+                    {file_path} 
+                    """,
+            "parse_mode": "Markdown"
+        }
+    else:
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": f"""
+            🚀 *Crawl Job Status: Hoàn thành thành công!*
+
+            🕒 Thời gian: `{crawl_time}`
+            📄 File kết quả: `{file_path}`
+            ✅ Tổng số bản ghi: *{total_records}*
+
+            ⚙️ Thông tin bổ sung:
+            - Thời gian chạy: {elapsed_time:.2f} giây
+            - Số lỗi phát sinh: {error_count}
+
+            💡 Lưu ý:
+            - Kiểm tra kỹ dữ liệu để đảm bảo tính chính xác.
+            - Nếu cần hỗ trợ, liên hệ team dev.
+
+            Cảm ơn bạn đã sử dụng bot crawl tự động!
+
+            🔗 Tải file: [Download tại đây]👇 
+            """,
+            "parse_mode": "Markdown"
+        }
+
     response = requests.post(url, data=payload)
     if response.status_code == 200:
         logger.error("✅ Gửi Telegram thành công.")
